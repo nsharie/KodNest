29858c059fd403d0bdfae3e859d3f1a382619f10
Nawaz Sharief <112409617+nsharie@users.noreply.github.com>
2023-07-16 21:51:10 +0530

    Commit

---
+++ com/kodnest/training/loops/PrintNumbers.java
@@ -0,0 +1,12 @@
+package com.kodnest.training.loops;
+
+public class PrintNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			System.out.println("KodNest:" + i);
+		}
+	}
+
+}
